91826b8b8a21c289ae9c72c627ea3c3b0257fb5c
Hujefa <huzefakazi2004@gmail.com>
2026-05-27 18:44:19 +0530

    terraform file added

--- ec2.tf
+++ ec2.tf
@@ -4,10 +4,10 @@ access_key = "afafa"
 secret_key = "sfsfs"
 }
 resource "aws_instance" "myec2" {
- ami = "ami-091138d0f0d41ff90"
-       instance type = "t3.micro"
-  key_name = "terraform-key"
-  vpc_security_groups_ids = [sg-097f80ca2a87eb65f]
+ami = "ami-091138d0f0d41ff90"
+instance type = "t3.micro"
+key_name = "terraform-key"
+vpc_security_groups_ids = ["sg-097f80ca2a87eb65f"]
 tags = {
 Name = "myec2"
 name = "huzefa"
