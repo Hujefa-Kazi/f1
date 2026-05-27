f592540c7c19a0b01ae2fad0cff687205119d748
Hujefa <huzefakazi2004@gmail.com>
2026-05-27 11:18:53 +0530

    first terraform file added

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "afafa"
+secret_key = "sfsfs"
+}
+resource "aws_instance" "myec2" {
+ ami = "ami-091138d0f0d41ff90"
+       instance type = "t3.micro"
+  key_name = "terraform-key"
+  vpc_security_groups_ids = [sg-097f80ca2a87eb65f]
+tags = {
+Name = "myec2"
+name = "huzefa"
+env = "dev"  
+}
+}
