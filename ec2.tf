92e3d5bc799925e115847fce87a46541aea83b66
Hujefa <huzefakazi2004@gmail.com>
2026-05-28 11:19:19 +0530

    terraform data added successfully

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,15 @@
 provider "aws" {
-region = "us-east-1"
-access_key = "afafa"
-secret_key = "sfsfs"
+  region = "us-east-1"
 }
+
 resource "aws_instance" "myec2" {
-ami = "ami-091138d0f0d41ff90"
-instance_type = "t3.micro"
-key_name = "terraform-key"
-vpc_security_group_ids = ["sg-097f80ca2a87eb65f"]
-tags = {
-Name = "myec2"
-name = "huzefa"
-env = "dev"  
-}
-}
+  ami                    = "ami-091138d0f0d41ff90"
+  instance_type          = "t3.micro"
+  key_name               = "terraform-key"
+  vpc_security_group_ids = ["sg-097f80ca2a87eb65f"]
 
-
+  tags = {
+    Name = "myec2"
+    Env  = "dev"
+  }
+}
