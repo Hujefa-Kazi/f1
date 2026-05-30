a2f4c7dda1788cd73a0246883be3304376a4bc7e
Hujefa <huzefakazi2004@gmail.com>
2026-05-30 07:40:41 +0530

    vpc added

--- ec2.tf
+++ ec2.tf
@@ -12,25 +12,29 @@ resource "aws_instance" "myec2" {
   ami                    = var.ami
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = [var.my-security]
+  vpc_security_group_ids = [var.my_security]
+
   tags = {
-    Name = var.instance-name
-    env  = "dev"
+    Name = var.instance_name
+    Env  = "dev"
   }
 }
-data "aws_security_groups" "my-security" {
+
+data "aws_security_group" "my_security" {
   filter {
-    name = "vpc-id"
+    name   = "vpc-id"
     values = [var.vpc_id]
   }
 }
+
 resource "aws_instance" "myec2_secondary" {
   ami                    = var.ami
   instance_type          = local.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids = data.aws_security_group.my-security.id
+  vpc_security_group_ids = [data.aws_security_group.my_security.id]
+
   tags = {
     Name = local.Name
-    Env  = "dev" 
+    Env  = "dev"
   }
-  }
+}
